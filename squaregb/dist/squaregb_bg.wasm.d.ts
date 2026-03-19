476739adbb8c8b6f75b115e09cd72ebea174fe1b
Ben Dudiak-Fry <bdudiakfry@gmail.com>
2026-03-19 12:32:40 +0000

    Build wasm artifacts in github actions instead of committing

--- squaregb/dist/squaregb_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const main: () => void;
-export const say_hello: () => [number, number];
-export const __externref_table_alloc: () => number;
-export const __wbindgen_externrefs: WebAssembly.Table;
-export const __wbindgen_free: (a: number, b: number, c: number) => void;
-export const __wbindgen_start: () => void;
