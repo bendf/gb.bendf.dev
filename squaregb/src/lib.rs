9147b41bea0ce5aa7af129fd61145414b98a2902
Ben Dudiak-Fry <bdudiakfry@gmail.com>
2026-03-24 11:23:26 +0000

    Decode LoadImm

--- squaregb/src/lib.rs
+++ squaregb/src/lib.rs
@@ -1,6 +1,16 @@
-use arbitrary_int::u2;
+use arbitrary_int::{u2, u3};
 use wasm_bindgen::prelude::*;
 use web_sys;
+
+type Reg = u8;
+const A: Reg = 0b111;
+const B: Reg = 0b000;
+const C: Reg = 0b001;
+const D: Reg = 0b010;
+const E: Reg = 0b011;
+const F: Reg = 0b100;
+const H: Reg = 0b101;
+const L: Reg = 0b110;
 
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
@@ -36,32 +46,51 @@ impl Machine {
         0xFF
     }
 }
-#[derive(Debug, PartialEq)]
-#[repr(u8)]
-pub enum Reg {
-    B = 0,
-}
 
 #[derive(Debug, PartialEq)]
 pub enum Instruction {
     LoadReg { src: Reg, dest: Reg },
-    // LoadImm { dest: Reg, imm: u8 },
+    LoadImm { dest: Reg, imm: u8 },
 }
 
 #[derive(Debug, PartialEq)]
-pub struct DecodeError(String);
+pub enum DecodeError {
+    UnknownOpcode,
+    MemoryOutOfBounds,
+}
 
 impl Instruction {
-    pub fn decode(opcode: u8) -> Result<Instruction, DecodeError> {
-        let b7_6 = u2::extract_u8(opcode, 6);
+    pub fn decode(memory: &[u8]) -> Result<Instruction, DecodeError> {
+        let Some(first_byte) = memory.first() else {
+            return Err(DecodeError::MemoryOutOfBounds);
+        };
 
-        if b7_6 == u2::new(0b01) {
-            Ok(Instruction::LoadReg {
-                src: Reg::B,
-                dest: Reg::B,
-            })
-        } else {
-            Err(DecodeError(String::from("Unknown opcode")))
+        let b7_6: u8 = u2::extract_u8(*first_byte, 6).value();
+        let b2_0: u8 = u3::extract_u8(*first_byte, 0).value();
+
+        match (b7_6, b2_0) {
+            (0b01, _) => {
+                let dest = u3::extract_u8(*first_byte, 3).value();
+                let src = u3::extract_u8(*first_byte, 0).value();
+
+                Ok(Instruction::LoadReg {
+                    src: src,
+                    dest: dest,
+                })
+            }
+            (0b00, 0b110) => {
+                let dest = u3::extract_u8(*first_byte, 3).value();
+
+                let Some(second_byte) = memory.get(1) else {
+                    return Err(DecodeError::MemoryOutOfBounds);
+                };
+
+                Ok(Instruction::LoadImm {
+                    dest,
+                    imm: *second_byte,
+                })
+            }
+            _ => Err(DecodeError::UnknownOpcode),
         }
     }
 }
@@ -71,29 +100,45 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_decodes_load_opcode() {
-        let opcode = 0b0100_0000;
-        let decoded = Instruction::decode(opcode).expect("LD should decode");
+    fn it_decodes_load_reg_opcode() {
+        let memory = [0b0100_0001];
+        let decoded = Instruction::decode(&memory).expect("LoadReg should decode");
 
-        assert_eq!(
-            decoded,
-            Instruction::LoadReg {
-                src: Reg::B,
-                dest: Reg::B
-            }
-        );
+        assert_eq!(decoded, Instruction::LoadReg { src: C, dest: B });
+    }
+
+    #[test]
+    fn it_decodes_load_imm_opcode() {
+        let memory = [0b00_111_110, 0b0000_0001];
+        let decoded = Instruction::decode(&memory).expect("LoadImm should decode");
+
+        assert_eq!(decoded, Instruction::LoadImm { dest: A, imm: 1 });
     }
 
     #[test]
     fn it_fails_to_decode_garbage() {
-        let opcode = 0b0000_0000;
-        let decoded = Instruction::decode(opcode);
+        let memory = [0b0000_0000];
+        let decoded = Instruction::decode(&memory);
 
         assert!(decoded.is_err());
 
-        assert_eq!(
-            decoded.unwrap_err(),
-            DecodeError(String::from("Unknown opcode"))
-        );
+        assert_eq!(decoded.unwrap_err(), DecodeError::UnknownOpcode);
+    }
+
+    #[test]
+    fn it_fails_to_decode_empty_memory() {
+        let memory: [u8; 0] = [];
+
+        let decoded = Instruction::decode(&memory);
+
+        assert_eq!(decoded.unwrap_err(), DecodeError::MemoryOutOfBounds);
+    }
+    #[test]
+    fn it_fails_to_decode_operand_out_of_bounds() {
+        let memory: [u8; 1] = [0b00_111_110];
+
+        let decoded = Instruction::decode(&memory);
+
+        assert_eq!(decoded.unwrap_err(), DecodeError::MemoryOutOfBounds);
     }
 }
