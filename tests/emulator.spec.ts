37a3f4f6bb79e14cf2ba73eb9ee0860faa535c99
Ben Dudiak-Fry <bdudiakfry@gmail.com>
2026-04-16 15:20:49 +0100

    Create (failing) test to test running a checkerboard ROM

--- tests/emulator.spec.ts
+++ tests/emulator.spec.ts
@@ -5,16 +5,58 @@ test("Emulator displays logo on boot", async ({ page }) => {
 
   // Give it a second to load
   page.waitForTimeout(1000);
-  const topLeft = await page.evaluate(() => {
-    const screen = document.getElementById("#squaregb-screen");
+
+  throw "TODO: Figure out what should be displayed!";
+});
+
+// Emulator displays black in to-right-hand corner
+test("Emulator test rom shows checkerboard", async ({ page }) => {
+  await page.goto("/");
+
+  const black = [0x00, 0x00, 0x00, 0x01];
+  const white = [0xff, 0xff, 0xff, 0x01];
+
+  page.waitForTimeout(1000);
+
+  await page.getByLabel("Load Checkerboard ROM").click();
+
+  await page.getByText("Run").click();
+
+  page.waitForTimeout(1000);
+
+  const firstTileColor = await page.evaluate(() => {
+    const screen = document.getElementById("squaregb-screen");
 
     if (screen instanceof HTMLCanvasElement) {
       const context = screen.getContext("2d")!;
 
       const imgData = context.getImageData(0, 0, 1, 1);
-      return imgData.data[0];
+      return [
+        imgData.data[0],
+        imgData.data[1],
+        imgData.data[2],
+        imgData.data[3],
+      ];
     }
   });
 
-  expect(topLeft).toBe(100);
+  expect(firstTileColor).toBe(black);
+
+  const secondTileColor = await page.evaluate(() => {
+    const screen = document.getElementById("squaregb-screen");
+
+    if (screen instanceof HTMLCanvasElement) {
+      const context = screen.getContext("2d")!;
+
+      const imgData = context.getImageData(8, 0, 1, 1);
+      return [
+        imgData.data[0],
+        imgData.data[1],
+        imgData.data[2],
+        imgData.data[3],
+      ];
+    }
+  });
+
+  expect(secondTileColor).toBe(white);
 });
